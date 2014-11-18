6e30077804d8aaf001abdbc40478e52490b54d86
Brenda Praggastis <bpraggastis@gmail.com>
2014-11-18 13:36:19 -0800

    created user model, session, and schema for users and credentials

--- LavenderBlush/config/routes.rb
+++ LavenderBlush/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   root "home#index"
-  
+
   post "/auth/:developer/callback", to: "session#create"
+  get "/sessions/welcome/:id", to: "session#welcome", as: :welcome
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
