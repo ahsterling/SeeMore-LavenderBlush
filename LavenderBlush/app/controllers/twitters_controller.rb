7ec2e8db9b7d0b13dff1ad620451a773a50696c0
Allie Sterling <sterlina89@gmail.com>
2014-11-18 11:40:17 -0800

    adds twitter-search form and routes, adds search box to layout

--- LavenderBlush/app/controllers/twitters_controller.rb
+++ LavenderBlush/app/controllers/twitters_controller.rb
@@ -1,6 +1,6 @@
 class TwittersController < ApplicationController
 
-  def search
+  def index
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
@@ -8,6 +8,17 @@ class TwittersController < ApplicationController
       config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
     end
     @bookis_results = client.user_search("bookis")
+  end
+
+  def search
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+    @results = client.user_search(params[:username])
+    
   end
 
 end
--- LavenderBlush/config/routes.rb
+++ LavenderBlush/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get "/twitter-search", to: "twitters#search"
+  get "/twitter-search", to: "twitters#index", as: :twitter_search
+  post "/twitter-search", to: "twitters#search"
 
   post "/auth/:developer/callback", to: "session#create"
 
