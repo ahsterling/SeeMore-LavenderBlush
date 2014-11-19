deedaef5a6f4b6a60240699b604bc05cf8bf1048
Allie Sterling <sterlina89@gmail.com>
2014-11-19 13:47:03 -0800

    adds validations to feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,4 +2,7 @@ class Feed < ActiveRecord::Base
 
   has_many :user_feeds
   has_many :users, through: :user_feeds
+
+  validates :handle, :provider, presence: true
+  validates :provider_uid, presence:true, numericality: true
 end
