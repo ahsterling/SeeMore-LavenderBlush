c177bf4529527d8d6e01610e72476a53634e4197
Brenda Praggastis <bpraggastis@gmail.com>
2014-11-19 09:57:22 -0800

    added comments to searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -30,7 +30,9 @@ class SearchesController < ApplicationController
   end
 
   def twitter_search
-    client = twitter_client
+    client = twitter_client  
+    # username is the variable twitter uses to represent the twitter handle
+    # for their searches. user_search is a twitter method
     @results = client.user_search(params[:username], { count: 10 })
     if @results.empty?
       @bookis = client.user(8553052)
