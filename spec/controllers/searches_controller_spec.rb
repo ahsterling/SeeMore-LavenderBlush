3a6da507199ecaab5d7547f9079a26023aaeb60d
Allie Sterling <sterlina89@gmail.com>
2014-11-19 11:03:15 -0800

    added search controller tests for not logged in

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -4,40 +4,50 @@ describe SearchesController do
 
   describe 'GET "index"' do
     it 'is successful' do
-      get :index
+      get :index, nil, {user_id: "123"}
       expect(response.status).to eq 200
+    end
+
+    it 'redirects when not logged in' do
+      get :index
+      expect(response).to redirect_to(root_path)
     end
   end
 
   describe 'POST "results"' do
     context 'Twitter search' do
       it 'displays results page with valid provider' do
-        post :results, username: "Bookis", provider: "Twitter"
+        post :results, {username: "Bookis", provider: "Twitter"}, {user_id: "123"}
         expect(response.status).to eq 200
       end
 
+      it 'redirects when not logged in' do
+        post :results, {username: "Bookis", provider: "Twitter"}
+        expect(response).to redirect_to(root_path)
+      end
+
       it 'does not display results page with invalid provider' do
-        post :results, username: "Bookis", provider: "foobar"
+        post :results, {username: "Bookis", provider: "foobar"}, {user_id: "123"}
         expect(response).to redirect_to(search_path)
       end
 
       it 'does not display results page without provider' do
-        post :results, username: "Bookis"
+        post :results, {username: "Bookis"}, {user_id: "123"}
         expect(response).to redirect_to(search_path)
       end
 
       it 'catches no search query' do
-        post :results, username: "", provider: "Twitter"
+        post :results, {username: "", provider: "Twitter"}, {user_id: "123"}
         expect(response).to redirect_to(search_path)
       end
 
       it 'sets @bookis if no results found' do
-        post :results, username: "aawegawg920j2vn8g2479913sj0x", provider: "Twitter"
+        post :results, {username: "aawegawg920j2vn8g2479913sj0x", provider: "Twitter"}, {user_id: "123"}
         expect(assigns(:bookis)).to be_a Twitter::User
       end
 
       it 'does not set @bookis if results found' do
-        post :results, username: "bookis", provider: "Twitter"
+        post :results, {username: "bookis", provider: "Twitter"}, {user_id: "123"}
         expect(assigns(:bookis)).to eq nil
       end
     end
