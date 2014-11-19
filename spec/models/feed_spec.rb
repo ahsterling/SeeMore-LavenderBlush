7f174e292fdeb275dcc5bc2a8c7ce3f5f55760a1
Allie Sterling <sterlina89@gmail.com>
2014-11-19 14:15:15 -0800

    adds tests to feed_spec validating attributes

--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Feed, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Feed do
+
+  describe ".validates" do
+    it 'saves a correct Feed' do
+      expect(Feed.new(provider: "Twitter", provider_uid: 123, handle: "bookis").valid?).to eq true
+    end
+
+    it "doesn't save a Feed with missing handle" do
+      expect(Feed.new(provider: "Twitter", provider_uid: 123).valid?).to eq false
+    end
+
+    it "doesn't save a Feed with a missing provider" do
+      expect(Feed.new(provider_uid: 123, handle: "bookis").valid?).to eq false
+    end
+
+    it "doesn't save a Feed with a missing provider_uid" do
+      expect(Feed.new(provider: "Twitter", handle: "bookis").valid?).to eq false
+    end
+
+    it "doesn't save a Feed with provider_uid that is not an integer" do
+      expect(Feed.new(provider: "Twitter", provider_uid: "blah", handle: "bookis").valid?).to eq false
+    end
+  end
+
 end
