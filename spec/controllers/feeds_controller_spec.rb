5a6201ed89056dc73987c6872f15d16c5b1e3e5a
Allie Sterling <sterlina89@gmail.com>
2014-11-19 14:34:14 -0800

    adds one feedscontroller test

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe FeedsController do
-
+  describe "POST #create" do
+    it 'is suceessful with a valid record' do
+      post :create, {add_form: {handle: "bookis", provider: "Twitter", provider_uid: 123}}, {user_id: 123}
+      expect(response).to redirect_to(welcome_path) 
+    end
+  end
   # creates new Feed when it doesn't already exist
   # creates new UserFeed when new Feed
 
