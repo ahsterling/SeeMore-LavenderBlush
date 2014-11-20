862d44c8d5d798afb68a56bddacdc58502e3a2a0
Allie Sterling <sterlina89@gmail.com>
2014-11-20 12:03:04 -0800

    deleted comments from post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :feed
   has_many :users, through: :feeds
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,50 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+case Rails.env
+when "development"
 
-fred = User.create(name: "fredflintstone", email: "fred@bedrock.com")
 
-feed1 = Feed.create(handle: "perolovkindgren", provider: "Vimeo")
-feed2 = Feed.create(handle: "user1557244", provider: "Vimeo")
-feed3 = Feed.create(handle: "sunflowertones" , provider: "Vimeo" )
+  feed_list = [
+    ["bookis", "Twitter", 8553052],
+    ["cuteemergency", "Twitter", 568825492],
+    ["fearlessideas", "Twitter", 84493112],
+    ["colemanfilm", "Vimeo", ]
+  ]
 
-uf1 = UserFeed.create(user_id: fred.id, feed_id: feed1.id)
-uf2 = UserFeed.create(user_id: fred.id, feed_id: feed2.id)
-uf3 = UserFeed.create(user_id: fred.id, feed_id: feed3.id)
+  feed_list.each do |handle, provider, provider_uid|
+    Feed.create(handle: handle, provider: provider, provider_uid: provider_uid)
+    Post.create()
+  end
+
+  user_list = [
+    ["Veronica Mars", "veronica@mars.com"],
+    ["Fred Flintstone", "fred@bedrock.com"],
+    ["Buffy Summers", "buffy@sunnydale.com"],
+  ]
+
+  user_list.each do |name, email|
+    user = User.create(name: name, email: email)
+    Credential.create(user_id: user.id, provider: "Developer")
+  end
+
+  user_feed_list = [
+    [1, 1],
+    [2, 2],
+    [3, 3]
+  ]
+
+  user_feed_list.each do |user_id, feed_id|
+    UserFeed.create(user_id: user_id, feed_id: feed_id)
+  end
+end
+
+# fred = User.create(name: "fredflintstone", email: "fred@bedrock.com")
+#
+# feed1 = Feed.create(handle: "perolovkindgren", provider: "Vimeo")
+# feed2 = Feed.create(handle: "user1557244", provider: "Vimeo")
+# feed3 = Feed.create(handle: "sunflowertones" , provider: "Vimeo" )
+#
+# uf1 = UserFeed.create(user_id: fred.id, feed_id: feed1.id)
+# uf2 = UserFeed.create(user_id: fred.id, feed_id: feed2.id)
+# uf3 = UserFeed.create(user_id: fred.id, feed_id: feed3.id)
