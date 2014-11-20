e1dda6855634fd9ff063a714f8898bcbc6c1dea1
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-19 16:32:21 -0800

    removing notes from feeds controller spec

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -57,12 +57,5 @@ describe FeedsController do
     end
 
   end
-  # creates new Feed when it doesn't already exist
-  # creates new UserFeed when new Feed
-
-  # doesn't create new Feed when Feed already exists
-  # creates new UserFeed when there's an existing Feed
-
-  # doesn't create new UserFeed when UserFeed already exists
 
 end
