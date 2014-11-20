391af23d268cda30c8b590dee8c2729a86bcf521
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-20 15:52:54 -0800

    fixing searches_controller tests

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe SearchesController do
 
+  let!(:test_user) {User.create(name: "cat")}
+
   describe 'GET "index"' do
+
     it 'is successful' do
-      get :index, nil, {user_id: "123"}
+      get :index, nil, {user_id: test_user.id}
       expect(response.status).to eq 200
     end
 
@@ -15,9 +18,10 @@ describe SearchesController do
   end
 
   describe 'POST "results"' do
+
     context 'Twitter search' do
       it 'displays results page with valid provider' do
-        post :results, {username: "Bookis", provider: "Twitter"}, {user_id: "123"}
+        post :results, {username: "Bookis", provider: "Twitter"}, {user_id: test_user.id}
         expect(response.status).to eq 200
       end
 
@@ -27,27 +31,27 @@ describe SearchesController do
       end
 
       it 'does not display results page with invalid provider' do
-        post :results, {username: "Bookis", provider: "foobar"}, {user_id: "123"}
+        post :results, {username: "Bookis", provider: "foobar"}, {user_id: test_user.id}
         expect(response).to redirect_to(search_path)
       end
 
       it 'does not display results page without provider' do
-        post :results, {username: "Bookis"}, {user_id: "123"}
+        post :results, {username: "Bookis"}, {user_id: test_user.id}
         expect(response).to redirect_to(search_path)
       end
 
       it 'catches no search query' do
-        post :results, {username: "", provider: "Twitter"}, {user_id: "123"}
+        post :results, {username: "", provider: "Twitter"}, {user_id: test_user.id}
         expect(response).to redirect_to(search_path)
       end
 
       it 'sets @bookis if no results found' do
-        post :results, {username: "aawegawg920j2vn8g2479913sj0x", provider: "Twitter"}, {user_id: "123"}
+        post :results, {username: "aawegawg920j2vn8g2479913sj0x", provider: "Twitter"}, {user_id: test_user.id}
         expect(assigns(:bookis)).to be_a Twitter::User
       end
 
       it 'does not set @bookis if results found' do
-        post :results, {username: "bookis", provider: "Twitter"}, {user_id: "123"}
+        post :results, {username: "bookis", provider: "Twitter"}, {user_id: test_user.id}
         expect(assigns(:bookis)).to eq nil
       end
     end
