1dd431f62fbad1bb4d3b0ca104405488f09af4e7
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-20 10:17:26 -0800

    playing around with pulling in latest tweets

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -26,6 +26,17 @@ class FeedsController < ApplicationController
     @videos = Vimeo::Simple::User.all_videos("perolovkindgren")
   end
 
+  def twitter # for testing, too!
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+
+    @tweets = client.user_timeline(8553052)
+  end
+
 
 end
 
