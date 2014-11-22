c57681fa1d7de0e0c794012aef29940f7017af77
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-21 22:32:42 -0800

    moving get_new_feed_posts from feeds_controller to post model

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,7 +8,7 @@ class FeedsController < ApplicationController
     if feed = Feed.find_existing_feed(params[:add_form])
       add_existing_feed(feed)
     else
-      feed = create_feed(params)
+      feed = create_new_feed(params)
       create_user_feed_and_posts(feed)
     end
   end
@@ -24,7 +24,7 @@ class FeedsController < ApplicationController
     end
   end
 
-  def create_feed(params)
+  def create_new_feed(params)
     feed = Feed.new(params.require(:add_form).permit(:provider, :provider_uid, :handle, :avatar))
     if feed.save
       feed
@@ -36,32 +36,10 @@ class FeedsController < ApplicationController
   def create_user_feed_and_posts(feed)
     user_feed = UserFeed.new(user_id: session[:user_id], feed_id: feed.id)
     if user_feed.save
-      get_new_feed_posts(feed)
+      Post.get_new_feed_posts(feed)
       redirect_to welcome_path, notice: "#{feed.handle} added to your feed!"
     else
       raise FeedError.new
-    end
-  end
-
-  def get_new_feed_posts(feed)
-    if feed.provider == "Twitter"
-      client = Twitter::REST::Client.new do |config|
-        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-        config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
-      end
-      client.user_timeline(feed.provider_uid).each do |tweet|
-        Post.create(date: tweet.created_at, text_content: tweet.text, feed_id: feed.id)
-      end
-    elsif feed.provider == "Vimeo"
-      Vimeo::Simple::User.all_videos(feed.provider_uid).each do |video|
-        Post.create(
-          date: video["upload_date"],
-          text_content: video["title"],
-          media_url: video["url"],
-          feed_id: feed.id)
-      end
     end
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,26 @@ class Post < ActiveRecord::Base
   belongs_to :feed
   has_many :users, through: :feeds
 
+  def self.get_new_feed_posts(feed)
+    if feed.provider == "Twitter"
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+      end
+      client.user_timeline(feed.provider_uid).each do |tweet|
+        Post.create(date: tweet.created_at, text_content: tweet.text, feed_id: feed.id)
+      end
+    elsif feed.provider == "Vimeo"
+      Vimeo::Simple::User.all_videos(feed.provider_uid).each do |video|
+        Post.create(
+        date: video["upload_date"],
+        text_content: video["title"],
+        media_url: video["url"],
+        feed_id: feed.id)
+      end
+    end
+  end
+
 end
