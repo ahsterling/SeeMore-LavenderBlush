c28e5edc985260b6eed9d5145b0d02e53c4d82e0
Brenda Praggastis <bpraggastis@gmail.com>
2014-11-24 11:27:57 -0800

    removed validation for name.

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,13 @@ Rails.application.routes.draw do
   get  "/auth/:developer/callback", to: "sessions#create"
   post "/auth/:developer/callback", to: "sessions#create"
 
-  get  "/auth/:provider/callback", to: "sessions#create"
-  post "/auth/:provider/callback", to: "sessions#create"
 
   # For testing:
   post "/sessions/create", to: "sessions#create"
   get "/sessions/welcome", to: "sessions#welcome", as: :welcome
 
   get "/users/:id",        to: "users#show", as: :user
-  
+
 
   post "/sessions/logout", to: "sessions#logout", as: :logout
 
