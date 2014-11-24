3e5ac833e3fe436d721149b48b88e4e09b6147c3
Bonnie <kbonnie@gmail.com>
2014-11-24 14:16:18 -0800

    more changes to application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,16 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+#Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module LavenderBlush
   class Application < Rails::Application
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
