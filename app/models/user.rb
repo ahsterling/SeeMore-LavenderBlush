8fb750aa727a7d40dac7ad9f37a9b95d1786154b
Allie Sterling <sterlina89@gmail.com>
2014-11-24 11:47:52 -0800

    other_providers method for user created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  validates :name, presence: true
+  # validates :name, presence: true
 
   has_many :user_feeds
   has_many :feeds, through: :user_feeds
@@ -10,6 +10,20 @@ class User < ActiveRecord::Base
 
   def has_feed?(result_id)
     self.feeds.collect { |f| f.provider_uid }.include?(result_id)
+  end
+
+  def other_providers
+    all_providers = ["twitter", "instagram", "vimeo", "github"]
+    existing_credentials = self.credentials.collect { |c| c.provider }
+    non_existing_credentials = []
+
+    all_providers.each do |p, index|
+      unless existing_credentials.include?(p)
+        non_existing_credentials << p
+      end
+    end
+
+    return non_existing_credentials
   end
 
 end
