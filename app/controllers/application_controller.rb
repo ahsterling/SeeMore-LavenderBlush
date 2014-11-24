a89f81206446b40dbe0606711f85c24f432918a0
Allie Sterling <sterlina89@gmail.com>
2014-11-24 13:34:30 -0800

    can add multiple credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+  helper_method :logged_in?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,25 +2,36 @@ class SessionsController < ApplicationController
   before_action :logged_in?, only: [:welcome]
 
   def create
-    # Find or create a user here
-    # Sign that user in
-    auth_hash = request.env['omniauth.auth']
-    user = User.includes(:credentials).references(:credentials).where("credentials.provider = '#{auth_hash.provider}' AND credentials.uid = '#{auth_hash.uid}'").first
 
-    if user == nil
-      user = User.new(
-              name: auth_hash.info.name,
-              email: auth_hash.info.email)
-      if user.save
-        Credential.create(
-              user_id: user.id,
-              provider: auth_hash.provider,
-              uid: auth_hash.uid)
-      else
-        redirect_to root_path
+    auth_hash = request.env['omniauth.auth']
+
+    if session[:user_id] == nil
+      user = User.includes(:credentials).references(:credentials).where("credentials.provider = '#{auth_hash.provider}' AND credentials.uid = '#{auth_hash.uid}'").first
+      if user == nil
+        user = User.new(
+                name: auth_hash.info.name,
+                email: auth_hash.info.email)
+        if user.save
+          Credential.create(
+                user_id: user.id,
+                provider: auth_hash.provider,
+                uid: auth_hash.uid)
+        else
+          redirect_to root_path
+        end
       end
+      session[:user_id] = user.id
+
+    else
+      Credential.create(user_id: current_user.id, provider: auth_hash.provider, uid: auth_hash.uid)
     end
-    session[:user_id] = user.id
+    # if logged_in?
+    #   Credential.create(
+    #   user_id: user.id,
+    #   provider: auth_hash.provider,
+    #   uid: auth_hash.uid)
+    # end
+
     redirect_to welcome_path, notice: "You have been successfully signed in."
   end
 
