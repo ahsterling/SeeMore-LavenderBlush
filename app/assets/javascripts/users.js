dd70666ebc990a5ed4a7c9fca9c104dee20a61ac
Allie Sterling <sterlina89@gmail.com>
2014-11-24 16:06:42 -0800

    clicking add feed changes button to red with text unsubscribe

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -20,15 +20,17 @@ $(function() {
     e.preventDefault();
     var feed = $(this).parents(".feed-wrapper");
     var form = $(this).closest("form");
-
     var url = form.attr("action");
-
 
     $.ajax(url, {
       type: "POST",
       data: form.serialize(),
       success: function() {
-        console.log("added");
+        var btn = form.find(".add");
+        btn.toggleClass("btn-danger btn-info");
+        btn.val("Unsubscribe");
+        form.children(".subscribe-button").toggleClass("unsubscribe-button subscribe-button");
+
       }
     });
 
