8a24a791302aef3d602658e83991f5e58a1b2801
Allie Sterling <sterlina89@gmail.com>
2014-11-25 11:27:59 -0800

    work in progress for feed subscribe and unsubscribe

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -10,90 +10,54 @@ $(function() {
     console.log(url);
 
     $.ajax("/user_feeds", {
-      type: "DELETE",
+      type: "POST",
       data: form.serialize(),
       success: function() {
         console.log("deleted");
+        feed.hide();
       }
 
     });
   });
-});
 
+  $(".subscribe").on("click", function(e) {
+    e.preventDefault();
+    var btn = $(this);
+    var feed = $(this).parents(".feed-wrapper");
+    var form = $(this).parents("form");
 
+    $.ajax("/feeds", {
+      type: "POST",
+      data: form.serialize(),
+      success: function() {
+        console.log("subscribe clicked");
+        btn.toggleClass("btn-danger btn-info");
+        btn.toggleClass("unsubscribe subscribe");
+        console.log(btn.attr("class"));
+        btn.val("Unsubscribe");
+        form.attr("action", "/user_feeds");
+      }
+    });
+  });
 
-// $(function() {
-//   $(".unsubscribe").click(function(e) {
-//     e.preventDefault();
-//     console.log(this);
-//     var feed = $(this).parents(".feed-wrapper");
-//     console.log(feed);
-//     var form = $(this).parents("form");
-//     console.log(form);
-//     var url = form.attr("action");
-//
-//     $.ajax("/user_feeds", {
-//       type: "POST",
-//       data: form.serialize(),
-//       success: function() {
-//         console.log("deleted");
-//         feed.hide();
-//       },
-//       failure: function() {
-//         console.log("fail");
-//       }
-//     });
-//   });
-
-  // $(".unsubscribe-button-search").click(function(e) {
-  //   console.log("Unsubscribe");
+  // $(".unsubscribe-results").on("click", function(e) {
   //   e.preventDefault();
-  //   console.log("Unsubscribe");
-  //   var feed = $(this).parents(".feed-wrapper");
-  //   var form = $(this).closest("form");
-  //   var url = form.attr("action");
-
+  //   var btn = $(this);
+  //   // var feed = $(this).parents(".feed-wrapper");
+  //   var form = $(this).parents("form");
+  //
   //   $.ajax("/user_feeds", {
   //     type: "POST",
   //     data: form.serialize(),
   //     success: function() {
-  //       var btn = form.find(".delete");
-  //       console.log(btn);
+  //       console.log("unsubscribe-results clicked");
+  //       btn.toggleClass("subscribe unsubscribe-results");
   //       btn.toggleClass("btn-info btn-danger");
-  //       btn.toggleClass("delete add");
   //       btn.val("Add Feed");
-  //       form.children(".unsubscribe-button-search").toggleClass("subscribe-button unsubscribe-button-search");
   //       form.attr("action", "/feeds");
-  //     },
-  //     failure: function() {
-  //       console.log('fail');
-  //     }
   //
-  //   });
-  //
-  // });
-  //
-  // $(".subscribe-button").click(function(e) {
-  //   e.preventDefault();
-  //   var feed = $(this).parents(".feed-wrapper");
-  //   var form = $(this).closest("form");
-  //   var url = form.attr("action");
-  //
-  //   $.ajax("/feeds", {
-  //     type: "POST",
-  //     data: form.serialize(),
-  //     success: function() {
-  //       var btn = form.find(".add");
-  //       btn.toggleClass("btn-danger btn-info");
-  //       btn.toggleClass("add delete");
-  //       btn.val("Unsubscribe");
-  //       form.children(".subscribe-button").toggleClass("unsubscribe-button-search subscribe-button");
-  //       form.attr("action", "/user_feeds");
-  //
-  //     },
-  //     failure: function() {
-  //       console.log("fail");
   //     }
   //   });
   // });
-// });
+
+});
