91a248a08ec0d8504190450e6cf181d23bdf8dd4
Allie Sterling <sterlina89@gmail.com>
2014-11-24 16:52:13 -0800

    more work on unsubscribe buttons

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -13,7 +13,30 @@ $(function() {
         feed.hide();
       }
     });
-    console.log("unsubscribe");
+  });
+
+  $(".unsubscribe-button-search").click(function(e) {
+    console.log("Unsubscribe");
+    e.preventDefault();
+    console.log("Unsubscribe");
+    var feed = $(this).parents(".feed-wrapper");
+    var form = $(this).closest("form");
+    var url = form.attr("action");
+
+    $.ajax("/user_feeds", {
+      type: "DELETE",
+      data: form.serialize(),
+      success: function() {
+        var btn = form.find(".delete");
+        console.log(btn);
+        btn.toggleClass("btn-info btn-danger");
+        btn.toggleClass("delete add");
+        btn.val("Add Feed");
+        form.children(".unsubscribe-button-search").toggleClass("subscribe-button unsubscribe-button-search");
+      },
+
+    });
+
   });
 
   $(".subscribe-button").click(function(e) {
@@ -28,11 +51,10 @@ $(function() {
       success: function() {
         var btn = form.find(".add");
         btn.toggleClass("btn-danger btn-info");
+        btn.toggleClass("add delete");
         btn.val("Unsubscribe");
-        form.children(".subscribe-button").toggleClass("unsubscribe-button subscribe-button");
-
+        form.children(".subscribe-button").toggleClass("unsubscribe-button-search subscribe-button");
       }
     });
-
   });
 });
