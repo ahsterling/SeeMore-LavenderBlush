269921a335aaeaf70bfd92030f0cc7a98d08f26a
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-26 11:55:17 -0800

    refactoring feeds_controller_spec

--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -5,9 +5,8 @@ describe FeedsController do
   let!(:test_user1) { User.create(name: "dog") }
   let!(:test_user2) { User.create(name: "pig") }
   let(:uid) { 8553052 }
-  let(:form_params) do
-    { add_form: { handle: "bookis", provider: "Twitter", provider_uid: uid } }
-  end
+  let(:form_params) { {add_form: {handle: "bookis", provider: "Twitter", provider_uid: uid}} }
+  let(:session_hash) { {user_id: test_user1.id} }
 
   describe "POST #create" do
 
@@ -19,22 +18,22 @@ describe FeedsController do
     end
 
     it 'redirects with an invalid record' do
-      post :create, {add_form: {provider: "Twitter", provider_uid: 123}}, {user_id: test_user1.id}
+      post :create, {add_form: {provider: "Twitter", provider_uid: 123}}, session_hash
       expect(response).to redirect_to(search_path)
     end
 
     context "creating and finding feeds" do
 
       it 'does not create duplicate feed' do
-        post :create, form_params, {user_id: test_user1.id}
+        post :create, form_params, session_hash
         expect {
-          post :create, form_params, {user_id: test_user1.id}
+          post :create, form_params, session_hash
         }.to change(Feed, :count).by(0)
       end
 
       it 'creates new Feed if not existing in database' do
         expect {
-          post :create, form_params, {user_id: test_user1.id}
+          post :create, form_params, session_hash
         }.to change(Feed, :count).by(1)
       end
 
@@ -42,27 +41,29 @@ describe FeedsController do
 
     context "creating userfeeds" do
 
-      # it 'does not create duplicate user feed' do
-      #   post :create, {add_form: {handle: "bookis", provider: "Twitter", provider_uid: uid}}, {user_id: test_user1.id}
-      #   user_feed_count = UserFeed.all.count
-      #   post :create, {add_form: {handle: "bookis", provider: "Twitter", provider_uid: uid}}, {user_id: test_user1.id}
-      #   expect(UserFeed.all.count).to eq user_feed_count
-      # end
+      it 'does not create duplicate user feed' do
+        post :create, form_params, session_hash
+        expect {
+          post :create, form_params, session_hash
+        }.to change(UserFeed, :count).by(0)
+      end
 
-      # it 'creates new UserFeed if not existing in database when only feed_id matches' do
-      #   post :create, {add_form: {handle: "bookis", provider: "Twitter", provider_uid: uid}}, {user_id: test_user2.id}
-      #   user_feed_count = UserFeed.all.count
-      #   post :create, {add_form: {handle: "bookis", provider: "Twitter", provider_uid: uid}}, {user_id: test_user1.id}
-      #   expect(UserFeed.all.count).to eq user_feed_count + 1
-      # end
+      it 'creates new UserFeed if not existing in database when only feed_id matches' do
+        post :create, form_params, {user_id: test_user2.id}
+        expect {
+          post :create, form_params, session_hash
+        }.to change(UserFeed, :count).by(1)
+      end
 
-      # it 'creates new UserFeed if not existing in database when only user_id matches' do
-      #   post :create, {add_form: {handle: "bookis", provider: "Twitter", provider_uid: uid}}, {user_id: test_user1.id}
-      #   feed_id = Feed.last.id
-      #
-      #   post :create, {add_form: {handle: "bookis", provider: "Vimeo", provider_uid: 123}}, {user_id: test_user1.id}
-      #   expect(Feed.last.id).to_not eq feed_id
-      # end
+      it 'creates new UserFeed if not existing in database when only user_id matches' do
+        post :create, form_params, session_hash
+        feed_id = Feed.last.id
+
+        expect {
+          post :create, {add_form: {handle: "bookis", provider: "Vimeo", provider_uid: 123}}, session_hash
+        }.to change(UserFeed, :count).by(1)
+        expect(Feed.last.id).to_not eq feed_id
+      end
 
     end
   end
@@ -70,11 +71,11 @@ describe FeedsController do
   describe "POST #update" do
     context "refreshing feeds" do
 
-      # it 'refreshes successfully' do
-      #   post :create, {add_form: {handle: "bookis", provider: "Twitter", provider_uid: uid}}, {user_id: test_user1.id}
-      #   post :update, {user_id: test_user1.id}
-      #   expect(response).to redirect_to welcome_path
-      # end
+      it 'refreshes successfully' do
+        post :create, form_params, session_hash
+        post :update, {user_id: test_user1.id}
+        expect(response).to redirect_to welcome_path
+      end
 
     end
   end
