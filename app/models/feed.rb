3a67acf92f9902be0060a1ff375d205c41460929
Kristina Hjertberg <hjert.k@gmail.com>
2014-11-26 16:39:13 -0800

    tried to fix bug. not successful yet

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -12,26 +12,25 @@ class Feed < ActiveRecord::Base
   end
 
   def self.bulk_happy(user_id)
-    happy_hash = {"785588682" => "emergency_fox",
-                  "19638752" => "NicestHippo",
-                  "568825492" => "CuteEmergency",
-                  "17045060" => "NationalZoo",
-                  "1337271" => "darth",
-                  "627799297" => "iamkidpresident",
-                  "15473958" => "MarsCuriosity",
-                  "740109097" => "SarcasticRover",
-                  "19725644" => "neiltyson",
-                  "549571045" => "FacesAndThings"
-    }
+    happy_hash = { "785588682" => "emergency_fox",
+                   "19638752" => "NicestHippo",
+                   "568825492" => "CuteEmergency",
+                   "17045060" => "NationalZoo",
+                   "1337271" => "darth",
+                   "627799297" => "iamkidpresident",
+                   "15473958" => "MarsCuriosity",
+                   "740109097" => "SarcasticRover",
+                   "19725644" => "neiltyson",
+                   "549571045" => "FacesAndThings" }
 
     happy_hash.each do |key, value|
       unless feed = self.find_existing_feed({ provider: "Twitter", provider_uid: key })
         feed = Feed.create(handle: value, provider: "Twitter", provider_uid: key)
       end
-      unless UserFeed.find_existing_user_feed({session: user_id}, feed)
+      unless UserFeed.find_existing_user_feed({session: {user_id: user_id}}, feed)
         UserFeed.create(feed_id: feed.id, user_id: user_id)
+        Post.get_new_feed_posts(feed)
       end
-      Post.get_new_feed_posts(feed)
     end
   end
 
