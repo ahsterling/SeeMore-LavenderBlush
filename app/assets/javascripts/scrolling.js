857dc21271dfaf7a9a608c620b88b4a777cadae3
Brenda Praggastis <bpraggastis@gmail.com>
2014-12-01 12:58:32 -0800

    created a function which watches for click without loading document

--- app/assets/javascripts/scrolling.js
+++ app/assets/javascripts/scrolling.js
@@ -1,12 +1,14 @@
-$(function(){
+var attach_events = function(){
 
   $(".pagination a").click(function(){
     var url = $(this).attr("href");
     console.log(url);
     $.getScript(url);
-    
     return false;
   });
 
+};
 
+$(function(){
+  attach_events();  
 });
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -33,7 +33,9 @@ class SessionsController < ApplicationController
 
   def welcome
     @user = User.find_by(id: session[:user_id])
-    @posts = @user.posts.order(date: :desc).paginate(page: params[:page]).per_page(5)
+    # @posts = @user.posts.order(date: :desc).paginate(page: params[:page]).per_page(5)
+    @posts = @user.posts.order(date: :desc).page(params[:page]).per_page(5)
+    
     # all_posts = SessionsHelper::FeedPage.new(@user, start)
     # @this_page = all_posts.this_page #based on start value this page will contain post at start index
     # @next_page = all_posts.next_page
