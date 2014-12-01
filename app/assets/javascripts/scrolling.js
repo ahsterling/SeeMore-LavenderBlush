1582c1d49f13c036b58887b15b64181d42de11ca
Brenda Praggastis <bpraggastis@gmail.com>
2014-11-30 21:15:22 -0800

    pagination works but no ajax

--- app/assets/javascripts/scrolling.js
+++ app/assets/javascripts/scrolling.js
@@ -1,12 +1,6 @@
 $(function(){
 
-  $(".pagination a").click(function(){
-    var url = $(this).attr("href");
-    console.log(url);
-    $.getScript(url);
-    
-    return false;
-  });
+
 
 
 });
